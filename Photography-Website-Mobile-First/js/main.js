e0ce9ed290d7bc4de5b30ce024870d3a2386dade
Manaswi Patil <manaswi.c.patil@gmail.com>
2022-04-03 10:23:24 +1200

    Photography website files

---
+++ Photography-Website-Mobile-First/js/main.js
@@ -0,0 +1,11 @@
+//Animate smooth scroll on view work button
+
+$('#view-work').on('click', function() {
+    const images = $('#images').position().top;
+
+    $('html, body').animate({
+            scrollTop: images
+        },
+        900
+    );
+});
